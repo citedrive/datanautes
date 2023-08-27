759cb772175ec6760ec8a86ea147654daf9b507e
garethbilaney <gareth.bilaney@hhu.de>
2023-08-27 12:50:29 +0200

    Update language-select.tsx

--- src/components/language-select.tsx
+++ src/components/language-select.tsx
@@ -1,34 +1,37 @@
 import type { ChangeEvent } from 'preact/compat';
 
 interface Props {
-    lang: string;
-  languages: {
-    [key: string]: string;
-  };
+  lang: string;
+languages: {
+  [key: string]: string;
+};
 }
 
 const LanguageSelector: React.FC<Props> = ({ lang, languages }) => {
-  return (
-    <label >
-        <select
-        id="languageSelector"
-        value={lang}
-        onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-          const newLang = e.currentTarget.value;
-          const [_leadingSlash, _oldLang, ...rest] = window.location.pathname.split('/');
-          const slug = rest.join('/');
-          window.location.pathname = `/${newLang}/${slug}`;
-        }}
-        className="bg-white text-gray-800 rounded-full px-4 py-2 shadow-lg focus:ring focus:ring-opacity-50"
-      >
-        {Object.entries(languages).map(([lang, label]) => (
-          <option key={lang} value={lang}>
-            {label}
-          </option>
-        ))}
-      </select>
-      </label>
-  );
+return (
+  <div>
+    <label htmlFor="languageSelector" className="block text-gray-700">
+      Select Language
+    </label>
+    <select
+      id="languageSelector"
+      value={lang}
+      onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+        const newLang = e.currentTarget.value;
+        const [_leadingSlash, _oldLang, ...rest] = window.location.pathname.split('/');
+        const slug = rest.join('/');
+        window.location.pathname = `/${newLang}/${slug}`;
+      }}
+      className="bg-white text-gray-800 rounded-full px-4 py-2 shadow-lg focus:ring focus:ring-opacity-50"
+    >
+      {Object.entries(languages).map(([lang, label]) => (
+        <option key={lang} value={lang}>
+          {label}
+        </option>
+      ))}
+    </select>
+  </div>
+);
 };
 
 export default LanguageSelector;
